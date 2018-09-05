aa3053dc33bd7839054c575d533e4025146e8e2a
erkin97 <matkaziev.97@gmail.com>
2018-09-06 00:57:20 +0900

    implement script for popup

--- popup.js
+++ popup.js
@@ -1,13 +1,6 @@
 $(function() {
-    var color = $('#fontColor').val();
-    $('#fontColor').on('change paste keyup', function(){
-        color = $(this).val();    
+    $('#btn_options').click(function (){
+        var win = window.open("./options.html", '_blank');
+        win.focus();
     });
-    $('#btnChange').click(function(){
-        chrome.tabs.query({active: true, currentWindow:true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id,
-            {todo: "changeColor", clickedColor: color});
-        });
-    });
-
 });
